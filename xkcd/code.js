0efbbc9c9fb5c1d71790aee4e1a9c456bee72370
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2025-03-25 09:43:38 +0100

    xkcd final

--- xkcd/code.js
+++ xkcd/code.js
@@ -9,7 +9,7 @@ function fetchIssue(num) {
         .then(data => {
             console.log("Fetched data:", data)
             current_number = data.num
-            document.getElementById("num").textContent = current_number
+            document.getElementById("num").placeholder = current_number
             document.getElementById("xkcd").children[0].src=data.img
             return data
         })
@@ -21,11 +21,16 @@ document.addEventListener('DOMContentLoaded',
             fetchIssue("latest").then(data => {latest_number = data.num})
         })
         document.getElementById("previous").addEventListener('click', function () {fetchIssue(current_number - 1)})
+
         document.getElementById("next").addEventListener('click', function () {
             console.log(current_number, latest_number)
             if (Number(current_number) < Number(latest_number)) { // vérification que le bouton next a un sens
                 fetchIssue(current_number + 1)}})
+        
 
-        }
+        document.getElementById("rechercher").addEventListener("click", function () { //prends en compte l'input de l'utilisateur
+            fetchIssue(document.getElementById("num").value)
+        
+        })}
     )
 // faire question 2
