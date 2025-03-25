14eecdb9aa1d966e0a4407ab0d03da76b5a6ef78
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2025-03-25 06:31:48 +0100

    xkcd presque fini

--- xkcd/code.js
+++ xkcd/code.js
@@ -1,20 +1,31 @@
 const XKCD = "https://xkcd.now.sh/?comic=";
+let current_number = ""
+let latest_number = ""
 
 function fetchIssue(num) {
     console.log("Fetching comic:", num);
-    fetch(XKCD + String(num))
+    return fetch(XKCD + String(num))
         .then(response => response.json())
         .then(data => {
             console.log("Fetched data:", data)
-            document.getElementById("num").textContent = data.num
+            current_number = data.num
+            document.getElementById("num").textContent = current_number
             document.getElementById("xkcd").children[0].src=data.img
+            return data
         })
 }
 
 document.addEventListener('DOMContentLoaded',
     function () {
         document.getElementById("reset").addEventListener('click', function () {
-            fetchIssue("latest")
+            fetchIssue("latest").then(data => {latest_number = data.num})
         })
+        document.getElementById("previous").addEventListener('click', function () {fetchIssue(current_number - 1)})
+        document.getElementById("next").addEventListener('click', function () {
+            console.log(current_number, latest_number)
+            if (Number(current_number) < Number(latest_number)) { // vérification que le bouton next a un sens
+                fetchIssue(current_number + 1)}})
+
         }
     )
+// faire question 2
