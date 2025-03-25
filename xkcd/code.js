d71feb3992ac19388e38dd2a3eabc5c9862679ee
Luigiromain <126761248+Luigiromain@users.noreply.github.com>
2025-03-25 05:43:34 +0100

    step 1 xkcd

--- xkcd/code.js
+++ xkcd/code.js
@@ -1,7 +1,20 @@
-const XKCD = "https://xkcd.now.sh/?comic="
+const XKCD = "https://xkcd.now.sh/?comic=";
 
 function fetchIssue(num) {
-    fetch(XKCD+String(num))
-    .then(response => response.json())
-    .then(data => console.log(data.num))
+    console.log("Fetching comic:", num);
+    fetch(XKCD + String(num))
+        .then(response => response.json())
+        .then(data => {
+            console.log("Fetched data:", data)
+            document.getElementById("num").textContent = data.num
+            document.getElementById("xkcd").children[0].src=data.img
+        })
 }
+
+document.addEventListener('DOMContentLoaded',
+    function () {
+        document.getElementById("reset").addEventListener('click', function () {
+            fetchIssue("latest")
+        })
+        }
+    )
